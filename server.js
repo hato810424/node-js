661acdb121dd63af619602e53cd5cacbac3d7f23
hato810424 <yukkuri.hatopoppo@gmail.com>
2022-06-14 22:05:54 +0900

    EJS対応

--- server.js
+++ server.js
@@ -1,24 +1,67 @@
-// WebServerを起動
+// 初期化
 var http = require('http');
-var html = require('fs').readFileSync('assets/index.html');
+var fs = require('fs');
+var ejs = require("ejs");
+let logs = Array();
 
-http = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
-    res.end(html);
-}).listen(8081, '0.0.0.0');
+// サーバー設定
+var server = http.createServer(function (req, res) {
+    // ページ振り分け
+    switch (req.url) {
+        case "/":
+            filename = "assets/index.ejs";
+            break;
+        default:
+            filename = "notfound"
+            break;
+    };
 
-// Socket.IO
-var io = require('socket.io')(http);
+    if(filename == "notfound"){
+        res.writeHead(404, {'Content-Type': 'text/plain;charset=utf-8'});
+        res.end("404 NotFound");
+    } else {
+        // ページレンダー
+        ejs.renderFile(filename, {
+            json_data: logs,
+        }, function (err, data) {
+            if (err) {
+                console.log({ err });
+                res.writeHead(404, { 'Content-Type': 'text/plain;charset=utf-8' });
+                res.end("エラーが発生しました");
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
+            res.write(data);
+            res.end();
+        });
+    };
+});
+
+// サーバー起動
+server.listen(8081, '0.0.0.0', function(){
+    console.log("起動");
+});
+
+//===========//
+// Socket.IO //
+//===========//
+var io = require('socket.io')(server);
+
+// 初期化
 var connect = 0;
 
 io.on('connection', (socket) => {
-    //接続
+    // 接続
     connect++;
     io.emit('count', connect);
+    io.emit('log', socket.id+"さんが接続しました。");
+    logs.push({"id":socket.id,"message":"接続"});
+    
 
-    //切断
+    // 切断
 	socket.on('disconnect', function () {
 		connect--;
         io.emit('count', connect); 
+        io.emit('log', socket.id+"さんが切断しました。");
+        logs.push({"id":socket.id,"message":"切断"});
 	});
 });
